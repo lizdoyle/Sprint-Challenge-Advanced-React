e31bfe1975ca756c6171384de7635bed4f21d3ca
lizdoyle <lizdoyle1121@gmail.com>
2019-09-15 22:33:20 -0400

    working on jest issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
+import PlayerCard from "./components/PlayerCard";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+
+    }
+
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+      .then((res) => {
+        this.setState({ players: res.data });
+        console.log("CDM in App.js:", this.state)
+      })
+      .catch(err => console.log(err))
+
+  }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.players !== this.state.players) {
+  //     .then((res) => {
+  //       this.setState({})
+  //     })
+  //    .catch(err => console.log(err))
+  //   }
+
+  // }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Top 100 Players</h1>
+        <PlayerCard players={this.state.players} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Header, Card, Icon } from "semantic-ui-react";
+
+function PlayerCard(props) {
+
+
+
+    console.log("PlayerCard Props", props.players)
+    return (
+        <div>
+            {props.players.map(player => {
+                const { name, id, country, searches } = player
+
+
+                return <Card className="playerCard" key={id} style={{
+                    border: '2px solid black',
+                    borderRadius: '10px',
+                    width: "300px",
+                    margin: '0 auto',
+                    marginBottom: '30px',
+                    boxSizing: 'border-box',
+                    padding: '20px'
+
+
+
+
+                }}>
+                    <Header as="h1">{name} </Header>
+                    <Card.Content id='ID:'>Player #: {id} </Card.Content>
+                    <Card.Meta content={country} />
+                    <Icon>Searches: {searches}</Icon>
+                </Card>
+
+            })}
+
+        </div>
+    )
+
+
+}
+
+
+
+
+export default PlayerCard
